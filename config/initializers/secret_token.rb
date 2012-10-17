f49acee7cb62d0922db0d44f36923e73a4fca0f3
Douglas Chan <douglaschan32167@berkeley.edu>
2012-10-17 01:05:10 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dcaport::Application.config.secret_token = 'b100fd124fcc9d19deec4c5fe08bdb1eae20ed4be561492ef1c52413a5174a17543a532ada3661b5e8f9d3d4e655f216a65e4c354223f6b922b8f2da9d874c20'
